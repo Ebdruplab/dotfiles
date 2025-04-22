8166b30f3ce04c0db11dab77987c0e37158dec9e
Kristian <43323270+kris9854@users.noreply.github.com>
2025-04-22 20:52:22 +0200

    Fixing submodules

--- linux/.config/zsh/exports.sh
+++ linux/.config/zsh/exports.sh
@@ -3,3 +3,14 @@
 # Ansible
 export ANSIBLE_TIMEOUT='200'
 
+# non secret
+export SSH_USER="${USERNAME}"
+# Fabric Exports
+# ---------------
+# Need ollama remote or local - https://www.ollama.com/
+# Need fabric - https://github.com/danielmiessler/fabric
+if command -v fabric &>/dev/null; then
+    export OPENAI_API_KEY="NULL"
+    export DEFAULT_MODEL="phi3:mini"
+    export OPENAI_BASE_URL=http://localhost:11434/api
+fi
