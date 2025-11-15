93539f712c39092c513940a54f38f31756b209f9
Kristian <43323270+kris9854@users.noreply.github.com>
2025-11-15 19:32:54 +0100

    Added mapping

--- linux/.config/nvim/lua/mappings.lua
+++ linux/.config/nvim/lua/mappings.lua
@@ -3,9 +3,10 @@ require "nvchad.mappings"
 -- add yours here
 
 -- Remove keymaps
--- Remove default nvchad
 vim.keymap.del("n", "<leader>e")
+
 local map = vim.keymap.set
+
 -- custom nvim open
 map("n", "\\", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 
@@ -13,12 +14,47 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Exit terminal mode and go to previous window
-vim.keymap.set('t', '<M-æ>', [[<C-\><C-n><C-w>p]], { noremap = true, silent = true })
+map('t', '<M-æ>', [[<C-\><C-n><C-w>p]], { noremap = true, silent = true })
 
 -- In normal mode, jump to the terminal window
-vim.keymap.set('n', '<M-æ>', '<C-w>p:startinsert<CR>', { noremap = true, silent = true })
+map('n', '<M-æ>', '<C-w>p:startinsert<CR>', { noremap = true, silent = true })
 
--- setup mapping to call :LazyGit
-vim.keymap.set('n', '<leader>gg', '<cmd>:LazyGit<CR>', { noremap = true, silent = true })
+-- LazyGit
+map('n', '<leader>gg', '<cmd>:LazyGit<CR>', { noremap = true, silent = true })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Ansible Vault keybindings
+
+-- Encrypt current file
+map("n", "<leader>ve", function()
+  local file = vim.fn.expand("%:p")
+  if file == "" then
+    print("No file to encrypt")
+    return
+  end
+
+  local cmd = "ansible-vault encrypt " .. file
+  print("Encrypting with: " .. cmd)
+
+  vim.fn.jobstart(cmd, {
+    on_exit = function(_, code)
+      if code == 0 then
+        print("Ansible Vault: Encryption successful!")
+      else
+        print("Ansible Vault: Encryption failed.")
+      end
+    end,
+  })
+end, { desc = "Encrypt file with Ansible Vault" })
+
+-- Decrypt current file
+map("n", "<leader>vd", function()
+  local file = vim.fn.expand("%:p")
+  vim.fn.jobstart("ansible-vault decrypt " .. file)
+end, { desc = "Decrypt file with Ansible Vault" })
+
+-- Vault edit
+map("n", "<leader>vv", function()
+  local file = vim.fn.expand("%:p")
+  vim.cmd("e! | silent !" .. "ansible-vault edit " .. file)
+end, { desc = "Ansible Vault edit" })
+
